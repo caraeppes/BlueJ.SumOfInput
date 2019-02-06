9cc85b1f0ebd7d727e646881d3084c0510f973df
Cara Eppes <caraeppes@zipcoders-MBP-4.fios-router.home>
2019-02-05 22:08:41 -0500

    edited main method so user can input a positive integer and program prints the sum of all integers from 1 to the input integer

--- Main.java
+++ Main.java
@@ -1,9 +1,28 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
 
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter a positive integer.");
+        
+        int input = scanner.nextInt();
+        
+        int sum = 0;
+        
+        for (int i = 1; i < input + 1; i++){
+            sum += i;
+        }
+        
+        System.out.println("The sum of the integers from 1 to " + input 
+        + " is " + sum + ".");
+        
+        
     }
 }
